088847905f1464041f0deb8f84861c5c1b1ff184
clemsciences <clemsciences@aol.com>
2018-07-11 23:42:12 +0200

    Initial commit

---
+++ sweden/data.py
@@ -0,0 +1,27 @@
+
+
+
+
+ramsund_runestone = "ᛋᛁᚱᚦ᛬ᚴᛁᛅᚱᚦᛁ᛬ᛒᚢᚦ᛬ᚦᚭᛋᛁ᛬ᛘᚢᚦᛁᛦ᛬ᛅᛚᚱᛁᚴᛋ᛬ᛏᚢᛏᛁᛦ᛬ᚢᚱᛘᛋ᛬ᚠᚢᚱ᛬ᛋᛅᛚᚢ᛬ᚼᛅᛚᚢ᛬ᚼᚢᛚᛘᚴᛁᚱᛋ᛬ᚠᛅᚦᚢᚱ᛬ᛋᚢᚴᚱᚢᚦᛅᚱ᛬ᛒᚢᛅᛏᛅ᛬ᛋᛁᛋ"
+
+
+# The Ed (Boulder) Inscription from Uppland, Sweden
+# http://runes.verbix.com/vikingage/Uppland.html
+ed_inscription = "ᚱᛅᚼᚾᚢᛅᛚᛏᚱ ᛫ ᛚᛁᛏ ᛫ ᚱᛁᛋᛏᛅ ᛫ ᚱᚢᚾᛅᚱ ᛫ ᚽᚡᛦ ᛫ ᚡᛅᛋᛏᚢᛁ ᛫ ᛘᚬᚦᚢᚱ ᛫ ᛋᛁᚾᛅ ᛫ ᚬᚾᚽᛘᛋ ᛫ ᛏᚬᛏᛦ ᛫ ᛏᚬ ᛁ ᛫ " \
+                 "ᛅᛁᚦᛁ ᛫ ᚴᚢᚦ ᚼᛁᛅᛚᛒᛁ ᛫ ᛅᚾᛏ ᛫ ᚼᚽᚾᛅ ᛫ " \
+                 "§B ᚱᚢᚾᛅ ᛫ ᚱᛁᛋᛏᛅ ᛫ ᛚᛁᛏ ᛫ ᚱᛅᚼᚾᚢᛅᛚᛏᚱ ᛫ ᚼᚢᛅᚱ ᛅ × ᚵᚱᛁᚴᛚᛅᚾᛏᛁ ᛫ ᚢᛅᛋ ᛫ ᛚᛁᛋ ᛫ ᚡᚬᚱᚢᚾᚴᛁ ᛫"
+
+# https://en.wikipedia.org/wiki/Uppland_Runic_Inscription_448
+u448 = "ᚴᚢᛚ᛫ᛅᚢᚴ᛫ᛒᛁᚢᚱᚾ᛫ᛚᛁᛏᚢ᛫ᚱᛅᛁᛋᛅ᛫ᛋᛏᛅᛁᚾ᛫ᛂᚠᛏᛁᛦ᛫ᚦᚢᚱᛋᛏᛅᛁᚾ᛫ᚠᛅᚦᚢᚱ"
+
+
+# https://en.wikipedia.org/wiki/Uppland_Runic_Inscription_92
+u92 = "ᚴᚾᚢᛏᚱ ' ᛁ ᚢᛁᚴ'ᚼᚢᛋᚢᛘ ' ᛚᛁᛏ ' ᛋᛏᛅᛁᚾ ' ᚱᛁᛏᛅ ' ᚢᚴ ' ᛒᚱᚬ ' ᚴᛁᚱᛅ ᛫ ᛁᚡᛏᛁᛦ ' ᚡᛅᚦᚢᚱ ᚢᚴ ᛫ ᛘᚬᚦᚬᚱ ᛫ ᚢᚴ ᛫" \
+      " ᛒᚱᚤᚦᚱ ᛫ ᛋᛁᚾᛅ ᛫ ᚢᚴ ᛫ ᛋᚢᛋᛏᚢᚱ"
+
+
+
+
+
+sweden_runic_inscription_filename = "sweden_runes.json"
+
---
+++ sweden/scraper.py
@@ -0,0 +1,41 @@
+"""
+
+"""
+
+import time
+import json
+from lxml import html
+import requests
+
+from .data import sweden_runic_inscription_filename
+
+__author__ = ["Clément Besnier <clemsciences@aol.com>", ]
+
+
+def retrieve_sweden_runic_inscription():
+    site = "http://runes.verbix.com/index.html"
+    rrunes = requests.get(site)
+    tree = html.fromstring(rrunes.text)
+    links = [i.get("href") for i in tree.xpath("//a") if "vikingage" in i.get("href")]
+    runes = {}
+    for link in links:
+        page = requests.get("http://runes.verbix.com/"+link)
+        tree = html.fromstring(page.text)
+        rune_blocks = tree.xpath('//p[@class="rrune"]')
+        print(len(rune_blocks))
+        content = [rb.text for rb in rune_blocks]
+        runes[link] = content
+        print(content)
+        time.sleep(1)
+    with open("sweden_runes.json", "w") as f:
+        json.dump(runes, f)
+
+    with open("sweden_runes.json", "r") as f:
+        loaded_runes = json.load(f)
+
+    print(loaded_runes.keys())
+
+
+if __name__ == "__main__":
+    retrieve_sweden_runic_inscription()
+    sweden_runic_inscription_filename()
